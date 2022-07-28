ef611be311a01daa8a04a7b650e2cb242b888565
Lauren Erxleben <lauren.n.erxleben@gmail.com>
2022-07-27 23:10:26 -0600

    commit tree 941b1a23640d704168547d0c7e9dd56ebe263842

--- app/models/network.rb
+++ app/models/network.rb
@@ -1,3 +1,9 @@
 class Network < ActiveRecord::Base
-  
+    #should return a list of all the show instances associated with the network.
+    has_many :shows
+
+#returns a string "We're sorry about passing on John Mulaney's pilot".
+    def sorry
+        "We're sorry about passing on John Mulaney's pilot"
+    end
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,15 @@
 class Show < ActiveRecord::Base
-  
+    #returns the characters associated with the show
+    # can be used to build its characters
+  has_many :characters
+  has_many :actors, through: :characters
+  #returns the network that the show belongs to
+  belongs_to :network
+
+  #Define a method in the Show class called #actors_list that returns an Array of the full names of each actor associated with a show. Remember, a show should have many actors through characters. Hint: it may help to use another association macro to access the actors for the show.
+  def actors_list
+    self.actors.map do |actor|
+        actor.full_name
+    end
+  end
 end
